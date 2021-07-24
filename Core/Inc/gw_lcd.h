7dfec76998ba7ed34877fff1abddc2406b3254e1
David Viens <plgDavid@users.noreply.github.com>
2021-07-24 10:27:38 -0400

    Fixed 8bit palette mode

--- Core/Inc/gw_lcd.h
+++ Core/Inc/gw_lcd.h
@@ -4,6 +4,8 @@
 #include "stm32h7xx_hal.h"
 #include <stdint.h>
 
+#define GW_LCD_MODE_LUT8
+
 #define GW_LCD_WIDTH  320
 #define GW_LCD_HEIGHT 240
 
@@ -11,21 +13,25 @@
 extern uint8_t framebuffer1[GW_LCD_WIDTH * GW_LCD_HEIGHT]  __attribute__((section (".lcd"))) __attribute__ ((aligned (16)));
 extern uint8_t framebuffer2[GW_LCD_WIDTH * GW_LCD_HEIGHT]  __attribute__((section (".lcd"))) __attribute__ ((aligned (16)));
 typedef uint8_t pixel_t;
+#define LCD_COLOR_MAX  0x100
+#define LCD_COLOR_MASK 0xFF
+#define LCD_COLOR_WHITE 0x1
+#define LCD_COLOR_GREY  0x2
 #else
 extern uint16_t framebuffer1[GW_LCD_WIDTH * GW_LCD_HEIGHT]  __attribute__((section (".lcd"))) __attribute__ ((aligned (16)));
 extern uint16_t framebuffer2[GW_LCD_WIDTH * GW_LCD_HEIGHT]  __attribute__((section (".lcd"))) __attribute__ ((aligned (16)));
 typedef uint16_t pixel_t;
+#define LCD_COLOR_MAX  0x10000
+#define LCD_COLOR_MASK 0xFFFF
+
+#define LCD_COLOR_WHITE 0xFFFF
+#define LCD_COLOR_GREY  0x0f0f
 #endif // GW_LCD_MODE_LUT8
-
-
-// To be shared between NES and GB. NES is larger.
-extern uint8_t emulator_framebuffer[(256 + 8 + 8) * 240]  __attribute__((section (".emulator_data")));
 
 
 // 0 => framebuffer1
 // 1 => framebuffer2
 extern uint32_t active_framebuffer;
-
 
 void lcd_deinit(SPI_HandleTypeDef *spi);
 void lcd_init(SPI_HandleTypeDef *spi, LTDC_HandleTypeDef *ltdc);
@@ -34,13 +40,13 @@ void lcd_backlight_on();
 void lcd_backlight_off();
 void lcd_swap(void);
 void lcd_sync(void);
-#define lcd_get_active_buffer(void) (active_framebuffer ? framebuffer2 : framebuffer1)
-#define lcd_get_inactive_buffer(void) (active_framebuffer ? framebuffer1 : framebuffer2)
 
+inline pixel_t* lcd_get_active_buffer  (void){return active_framebuffer ? framebuffer2 : framebuffer1;}
+inline pixel_t* lcd_get_inactive_buffer(void){return active_framebuffer ? framebuffer1 : framebuffer2;}
 
 // To be used by fault handlers
 void lcd_reset_active_buffer(void);
-void lcd_draw_text_6x6(int x, int y, const char* text, unsigned short color);
-void lcd_draw_text_8x8(int x, int y, const char* text, unsigned short color);
+void lcd_draw_text_6x6(int x, int y, const char* text, pixel_t color);
+void lcd_draw_text_8x8(int x, int y, const char* text, pixel_t color);
 
 #endif
